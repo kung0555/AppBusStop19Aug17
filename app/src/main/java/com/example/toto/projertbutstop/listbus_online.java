0732a082a73f71b6afc504204bb27e1cb7be6338
Kung <kung0555@gmail.com>
2017-08-19 12:59:03 +0700

    SearchFinish

--- app/src/main/java/com/example/toto/projertbutstop/listbus_online.java
+++ app/src/main/java/com/example/toto/projertbutstop/listbus_online.java
@@ -2,6 +2,7 @@ package com.example.toto.projertbutstop;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -9,20 +10,51 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class listbus_online extends AppCompatActivity {
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.Language;
+import com.akexorcist.googledirection.constant.RequestResult;
+import com.akexorcist.googledirection.constant.TransitMode;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.constant.Unit;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Step;
+import com.akexorcist.googledirection.util.DirectionConverter;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class listbus_online extends AppCompatActivity implements OnMapReadyCallback,DirectionCallback {
     double latSearch;
     double lngSearch;
     double latStartADouble;
     double lngStartADouble;
     private LocationManager locationManager;
-
+    private GoogleMap googleMap;
+    private String serverKey = "AIzaSyANztP01h4SRxFBJjiKLrxm5uWP1yCQr4E";
+    String x1;
+    String x2;
+    String x3;
+    String x4;
+    String x5;
+    String x6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listbus_online);
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapTest)).getMapAsync(this);
+
 
         //รับข้อมูล
         intentBusStop();
@@ -119,6 +151,106 @@ public class listbus_online extends AppCompatActivity {
         else if (gpsLocation == null) {
             Toast.makeText(getApplicationContext(), "ไม่มี GPS", Toast.LENGTH_SHORT).show();
         }
+        requestDirection();
+
+    }
+    private void requestDirection() {
+        LatLng origin = new LatLng(13.778512, 100.507915);
+        LatLng destination = new LatLng(13.787855, 100.490267);
+        GoogleDirection.withServerKey(serverKey)
+                .from(new LatLng(latStartADouble,lngStartADouble))
+                //.from(origin)
+                //.to(destination)
+                .to(new LatLng(latSearch, lngSearch))
+                .language(Language.THAI)
+                .transportMode(TransportMode.TRANSIT)
+                .unit(Unit.METRIC)
+                .transitMode(TransitMode.BUS)
+                .execute(this);
+
+    }
+
+    @Override
+    public void onDirectionSuccess(Direction direction, String rawBody) {
+        String status = direction.getStatus();
+        if (status.equals(RequestResult.OK)) {
+            Toast toast = Toast.makeText(this, "OK", Toast.LENGTH_LONG);
+            toast.show();
+
+
+            ArrayList<LatLng> sectionPositionList = direction.getRouteList().get(0).getLegList().get(0).getSectionPoint();
+            for (LatLng position : sectionPositionList) {
+                googleMap.addMarker(new MarkerOptions().position(position));
+            }
+
+            List<Step> stepList = direction.getRouteList().get(0).getLegList().get(0).getStepList();
+            ArrayList<PolylineOptions> polylineOptionList = DirectionConverter.createTransitPolyline(this, stepList, 5, Color.RED, 3, Color.BLUE);
+            for (PolylineOptions polylineOption : polylineOptionList) {
+                googleMap.addPolyline(polylineOption);
+            }
+
+            Button b1 = (Button) findViewById(R.id.buttOnline);
+            String z = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(0).getTravelMode();
+            String a = "WALKING";
+            boolean gg = z.equals(a);
+            int i = 0;
+            if (gg==true) {
+                i=1;
+                x1 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getLine().getShortName();
+                x2 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getStopNumber();
+                x3 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getLine().getName();
+                x4 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getDistance().getText();
+                x5 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getDepartureStopPoint().getName();
+                x6 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getArrivalStopPoint().getName();
+                b1.setText("รถประจำทางสาย " + x1);
+                /*tg1.setText("รถประจำทางสาย " + x1);
+                tg2.setText("ต้องผ่านทั้งหมด " + x2 + " ป้าย");
+                tg3.setText("วิ่งจาก " + x3);
+                tg4.setText("อีก " + x4);
+                tg5.setText("ป้ายรถประจำทางเริ่มต้น " +x5);
+                tg6.setText("ป้ายรถประจำทางปลายทาง "+x6);*/
+
+            }
+            else {
+                x1 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getLine().getShortName();
+                x2 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getStopNumber();
+                x3 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getLine().getName();
+                x4 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getDistance().getText();
+                x5 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getDepartureStopPoint().getName();
+                x6 = direction.getRouteList().get(0).getLegList().get(0).getStepList().get(i).getTransitDetail().getArrivalStopPoint().getName();
+                b1.setText("รถประจำทางสาย " + x1);
+
+            }
+        }
+        else if (status.equals(RequestResult.NOT_FOUND)) {
+            Toast toast = Toast.makeText(this, "NOT_FOUND", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (status.equals(RequestResult.ZERO_RESULTS)) {
+            Toast toast = Toast.makeText(this, "ไม่มีเส้นทาง", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (status.equals(RequestResult.MAX_WAYPOINTS_EXCEEDED)) {
+            Toast toast = Toast.makeText(this, "MAX_WAYPOINTS_EXCEEDE", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (status.equals(RequestResult.REQUEST_DENIED)) {
+            Toast toast = Toast.makeText(this, "REQUEST_DENIED", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (status.equals(RequestResult.UNKNOWN_ERROR)) {
+            Toast toast = Toast.makeText(this, "UNKNOWN_ERROR", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onDirectionFailure(Throwable t) {
+        Toast toast = Toast.makeText(this, "ไม่สามารถค้นหาได้", Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latSearch, lngSearch), 16));
 
     }
 }
