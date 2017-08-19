8983a426f6105d55601de0e3aa8f8b79891f4ea0
Kung <kung0555@gmail.com>
2017-08-20 00:20:36 +0700

    Start&End

--- app/src/main/java/com/example/toto/projertbutstop/listbus_offline.java
+++ app/src/main/java/com/example/toto/projertbutstop/listbus_offline.java
@@ -13,8 +13,11 @@ public class listbus_offline extends AppCompatActivity {
 
     private String startString, endString;
     private String[] numberBusStrings;
+    private String[] numberBusEndStrings;
     private ArrayList<String> myTrueNumberBusStartStringArrayListinTown;
     private ArrayList<String> myTrueNumberBusStartStringArrayListoutTown;
+    private ArrayList<String> myTrueNumberBusEndStringArrayListinTown;
+    private ArrayList<String> myTrueNumberBusEndStringArrayListoutTown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +26,85 @@ public class listbus_offline extends AppCompatActivity {
 
         myTrueNumberBusStartStringArrayListinTown = new ArrayList<String>();
         myTrueNumberBusStartStringArrayListoutTown = new ArrayList<String>();
-
+        myTrueNumberBusEndStringArrayListinTown = new ArrayList<String>();
+        myTrueNumberBusEndStringArrayListoutTown = new ArrayList<String>();
         //GetValue Intent
         getValueIntent();
 
         //Find NumberBus
-        findNumberBus();
+        findNumberBusStart();
+        findNumberBusEnd();
+
 
 
     }   // Main Method
 
-    private void findNumberBus() {
+    private void findNumberBusEnd() {
+        String tag = "18AugV4";
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM busrouteTABLE", null);
+        cursor.moveToFirst();
+        String[] strNumberBusManyStrings = new String[cursor.getCount()];
+        for (int i=0;i<strNumberBusManyStrings.length; i++) {
+            strNumberBusManyStrings[i] = cursor.getString(2);
+            Log.d(tag, "bus[" + i + "] ==> " + strNumberBusManyStrings[i]);
+            stringArrayList.add(strNumberBusManyStrings[i]);
+            cursor.moveToNext();
+        }   //for
+        cursor.close();
+        Log.d(tag, "stringArrayList ==> " + stringArrayList);
+        Object[] objects = stringArrayList.toArray();
+        for (Object object : objects) {
+            if (stringArrayList.indexOf(object) != stringArrayList.lastIndexOf(object)) {
+                stringArrayList.remove(stringArrayList.lastIndexOf(object));
+            }
+        }
+        Log.d(tag, "last StringArrayList ==> " + stringArrayList);
+        numberBusEndStrings = new String[stringArrayList.size()];
+        numberBusEndStrings = stringArrayList.toArray(new String[0]);
+        for (int i=0;i<numberBusEndStrings.length;i++) {
+            Log.d(tag, "numbutBus[" + i + "] ==> " + numberBusEndStrings[i]);
+
+            inTownCheckBusEnd(numberBusEndStrings[i], sqLiteDatabase);
+            outTownCheckBusEnd(numberBusEndStrings[i], sqLiteDatabase);
+
+        }   // for
+        Log.d("20AugV1", "รถที่วิ่งผ่านป้าย Endไป ==> " + myTrueNumberBusEndStringArrayListinTown);
+        Log.d("20AugV1", "รถที่วิ่งผ่านป้าย Endกลับ ==> " + myTrueNumberBusEndStringArrayListoutTown);
+    }// findNumberBusEnd
+
+    private void outTownCheckBusEnd(String numberBusString, SQLiteDatabase sqLiteDatabase) {
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM busrouteTABLE WHERE direction = 'ออกเมือง' AND bus = " + numberBusString, null);
+        cursor.moveToFirst();
+
+        for (int i=0;i<cursor.getCount();i++) {
+
+            if (startString.equals(cursor.getString(4))) {
+                myTrueNumberBusEndStringArrayListoutTown.add(numberBusString);
+            }
+            cursor.moveToNext();
+
+        }   // for
+    }
+
+    private void inTownCheckBusEnd(String numberBusString, SQLiteDatabase sqLiteDatabase) {
+
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM busrouteTABLE WHERE direction = 'เข้าเมือง' AND bus = " + numberBusString, null);
+        cursor.moveToFirst();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+
+            if (endString.equals(cursor.getString(4))) {
+                myTrueNumberBusEndStringArrayListinTown.add(numberBusString);
+            }
+            cursor.moveToNext();
+
+        }   // for
+    }
+
+    private void findNumberBusStart() {
         String tag = "18AugV4";
         ArrayList<String> stringArrayList = new ArrayList<String>();
         SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
@@ -68,11 +139,13 @@ public class listbus_offline extends AppCompatActivity {
 
         }   // for
 
-        Log.d(tag, "รถที่วิ่งผ่านป้าย Startไป ==> " + myTrueNumberBusStartStringArrayListinTown);
-        Log.d(tag, "รถที่วิ่งผ่านป้าย Startกลับ ==> " + myTrueNumberBusStartStringArrayListoutTown);
+        Log.d("20AugV1", "รถที่วิ่งผ่านป้าย Startไป ==> " + myTrueNumberBusStartStringArrayListinTown);
+        Log.d("20AugV1", "รถที่วิ่งผ่านป้าย Startกลับ ==> " + myTrueNumberBusStartStringArrayListoutTown);
 
 
-    }   // findNumberBus
+    }   // findNumberBusSart
+
+
 
     private void outTownCheckBus(String numberBusString, SQLiteDatabase sqLiteDatabase) {
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM busrouteTABLE WHERE direction = 'ออกเมือง' AND bus = " + numberBusString, null);
